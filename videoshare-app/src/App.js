46addc6f69bb13e9f001374d8826dc86c4401708
zsjp30831 <73112777+zsjp30831@users.noreply.github.com>
2021-02-09 17:15:24 +0900

    v1-3

--- videoshare-app/src/App.js
+++ videoshare-app/src/App.js
@@ -1,22 +1,23 @@
 import React, {Component} from 'react';
-// // import {Button, Icon} from 'antd-mobile';
-import logo from './admin-logo.png';
 import styles from './App.css';
-import LoginForm from "./user/LoginForm";
+import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+import Error from "./components/404";
+import {Route,Switch} from "react-router-dom"
 
 class App extends Component {
     render() {
         return (
-            <div className={styles.App}>
-                <header className={styles.App_header}>
-                    <h3 className={styles.App_title}>
-                        <img src={logo} className={styles.App_logo} alt="logo"/>Platform Demo Login
-                    </h3>
-                    <LoginForm/>
-                    <footer className={styles.App_footer}>@VRC, INC. All Rights Reserved.</footer>
-                </header>
+            <div>
+                <Switch >
+                    <Route exact path='/' component={LoginForm}/>
+                    <Route path='/home' component={Home}/>
+                    <Route component={Error}/>
+                </Switch>
             </div>
+
         );
+
     }
 }
 
---
+++ videoshare-app/src/components/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = ()=>{
+
+    return(
+        <div>
+            <p>this is 404</p>
+        </div>
+    );
+}
+
+export  default  Error;
