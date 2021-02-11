1d850414d553d4537bf3d4a053090fd1d26e7939
zsjp30831 <73112777+zsjp30831@users.noreply.github.com>
2021-02-11 13:57:45 +0900

    v1-7

--- videoshare-app/src/components/404.js
+++ videoshare-app/src/components/404.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Button} from "antd-mobile";
+import {fwPush} from "../common/common";
+import Style from '../App.css';
+import logo from "../image/404.png";
 
 const Error = ()=>{
 
     return(
-        <div>
-            <p>this is 404</p>
+        <div className={Style.App_header}>
+            <img src={logo} className={Style.App_logo} alt="logo"/>
+            <h3>404</h3>
+            <p>Sorry, the page you visited does not exist.</p>
+            <Button  size="small" type="primary" onClick={() => fwPush('/home')}>
+                Back Home
+            </Button>
+            <footer className={Style.footer}>
+                <span>Copyright © VRC, Inc. All Rights Reserved.</span>
+            </footer>
         </div>
     );
 }
