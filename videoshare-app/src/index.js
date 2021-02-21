11e4ac1009af4926d648602f80fb16d5c69ebc4b
zsjp30831 <73112777+zsjp30831@users.noreply.github.com>
2021-02-21 17:40:32 +0900

    v1-17-3

--- videoshare-app/src/index.js
+++ videoshare-app/src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter basename="/hus">
+    <BrowserRouter>
         <App/>
     </BrowserRouter>,
     document.getElementById('root'));
