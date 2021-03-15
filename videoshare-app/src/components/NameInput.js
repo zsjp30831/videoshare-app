d0384acddeb09a85d8944ff343e917b0b00e9d9d
zhangli-jerry <li.zhang@vrcjp.com>
2021-03-15 16:02:04 +0900

    src fix - add status check

--- videoshare-app/src/components/NameInput.js
+++ videoshare-app/src/components/NameInput.js
@@ -45,7 +45,13 @@ class NameInput extends Component {
                             fwLoading("動画作成しました。");
                             timer && clearInterval(timer);
                             fwPush('/webview', {vrcId: getVrcId(), relationId: getRelationId()});
-                        } else if (status !== 'Failed') {
+                        } else if (status == 'Ready') {
+                            pollingFlag = false;
+                            if (loadingFlag) {
+                                loadingFlag = false;
+                                fwLoading("動画作成準備中...");
+                            }
+                        } else if (status == 'Pending') {
                             pollingFlag = false;
                             if (loadingFlag) {
                                 loadingFlag = false;
